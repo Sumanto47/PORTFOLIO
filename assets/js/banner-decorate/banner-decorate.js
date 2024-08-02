36ff894f56dc136be9380a74efbf6241c4a1ae40
Sumanto47 <sumantochatterjeenavi47@gmail.com>
2024-08-02 16:57:12 +0530

    Add files via upload

---
+++ assets/js/banner-decorate/banner-decorate.js
@@ -0,0 +1,19 @@
+/*------------------------------------------- SCRIPT FOR BANNER-DECORATE START ------------------------------------------*/
+
+const words = '............................................................';
+const duration = 3000;
+const characters = words.split('').forEach((char, i) => {
+  function createElement(offset) {
+    const div = document.createElement('div');
+    div.innerText = char;
+    div.classList.add('character');
+    div.style.animationDelay = `-${i * (duration / 10) - offset}ms`
+    
+    return div;
+  }
+  document.getElementById('spiral').append(createElement(0));
+  
+  document.getElementById('spiral2').append(createElement(-1 * (duration / 2)));
+});
+
+/*------------------------------------------- SCRIPT FOR BANNER-DECORATE COMPLETE ------------------------------------------*/
